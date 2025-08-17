244a298c37ab1a3fdb5c95090b0b23c734d8b551
RealElvince <obuongely@yahoo.com>
2025-08-17 14:55:34 +0300

    function to generate fake user data

--- producers/producer.py
+++ producers/producer.py
@@ -5,3 +5,18 @@ from faker import Faker
 
 
 fake = Faker()
+
+def get_registered_user():
+    """Generate a fake registered user."""
+    return {
+        "username": fake.user_name(),
+        "email": fake.email(),
+        "password": fake.password(),
+        "first_name": fake.first_name(),
+        "last_name": fake.last_name(),
+        "address": fake.address(),
+        "phone_number": fake.phone_number(),
+        "created_at":fake.year()
+    }
+
+    
